08b045a3cceaed383de65df6e77a33e3a647039f
Bryan Christopher Johnson <bryanchristopherjohnson@gmail.com>
2020-06-03 16:44:14 -0400

    Add asm code for making a Test class file

---
+++ src/com/xarql/kdl/Test.java
@@ -0,0 +1,9 @@
+package com.xarql.kdl;
+
+public class Test {
+    public static final String OUT = "hello world";
+
+    public static void main(String[] args) {
+        System.out.println(OUT);
+    }
+}
---
+++ src/com/xarql/kdl/TestClassOut.java
@@ -0,0 +1,75 @@
+package com.xarql.kdl;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class TestClassOut implements Opcodes {
+
+    public static void main(String[] args) throws IOException, Exception {
+        File outLoc = new File(System.getProperty("user.home") + "/Documents/kdl/jvm/class");
+        outLoc.mkdirs();
+        outLoc = new File(outLoc, "/Test.class");
+        outLoc.createNewFile();
+        Files.write(outLoc.toPath(), dump());
+    }
+
+    public static byte[] dump() throws Exception {
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+        FieldVisitor fv;
+        MethodVisitor mv;
+        AnnotationVisitor av0;
+
+        cw.visit(V1_8, ACC_PUBLIC + ACC_SUPER, "com/xarql/kdl/Test", null, "java/lang/Object", null);
+
+        cw.visitSource("Test.java", null);
+
+        {
+            fv = cw.visitField(ACC_PUBLIC + ACC_FINAL + ACC_STATIC, "OUT", "Ljava/lang/String;", null, "hello world");
+            fv.visitEnd();
+        }
+        {
+            mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+            mv.visitCode();
+            Label l0 = new Label();
+            mv.visitLabel(l0);
+            mv.visitLineNumber(3, l0);
+            mv.visitVarInsn(ALOAD, 0);
+            mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+            mv.visitInsn(RETURN);
+            Label l1 = new Label();
+            mv.visitLabel(l1);
+            mv.visitLocalVariable("this", "Lcom/xarql/kdl/Test;", null, l0, l1, 0);
+            mv.visitMaxs(0, 0);
+            mv.visitEnd();
+        }
+        {
+            mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+            mv.visitCode();
+            Label l0 = new Label();
+            mv.visitLabel(l0);
+            mv.visitLineNumber(7, l0);
+            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitLdcInsn("hello world");
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+            Label l1 = new Label();
+            mv.visitLabel(l1);
+            mv.visitLineNumber(8, l1);
+            mv.visitInsn(RETURN);
+            Label l2 = new Label();
+            mv.visitLabel(l2);
+            mv.visitLocalVariable("args", "[Ljava/lang/String;", null, l0, l2, 0);
+            mv.visitMaxs(0, 0);
+            mv.visitEnd();
+        }
+        cw.visitEnd();
+
+        return cw.toByteArray();
+    }
+}
