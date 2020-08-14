ced8744a3de5bf23c017acdd3815706cd7d9582f
cliserkad <bryanchristopherjohnson@gmail.com>
2020-08-14 18:28:08 -0400

    Refactor tests to use StandardKdlTest

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,11 +1,11 @@
 package test.java;
 
+import com.xarql.kdl.BestList;
 import com.xarql.kdl.CompilationDispatcher;
 import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
-    public static final String FIZZBUZZ_CMD = StandardKdlTest.JAVA_CMD + "/sample FizzBuzz ";
     public static final int[] NUMS = { -50, -30, -15, -5, -3, -1, 0, 1, 3, 5, 15, 30, 40, 45, 50, 99, 22 };
 
     public static void main(String[] args) {
@@ -14,22 +14,14 @@ public class FizzBuzzTest {
 
     @Test
     public void testFizzBuzz() {
-        try {
-            // compile FizzBuzz.kdl
-            new CompilationDispatcher(new RegexFileFilter("FizzBuzz.kdl"), true).compileAll();
+        BestList<String> fizzBuzzArguments = new BestList<String>();
+        for(int n : NUMS)
+            fizzBuzzArguments.add("" + n);
+        BestList<String> fizzBuzzOutputs = new BestList<String>();
+        for(int n : NUMS)
+            fizzBuzzOutputs.add(fizzBuzz(n));
 
-            for(int n : NUMS)
-                fizzBuzzCase(n);
-
-        } catch(Exception e) {
-            e.printStackTrace();
-            assert false;
-        }
-    }
-
-    private void fizzBuzzCase(int input) throws Exception {
-        ProcessOutput process = ProcessOutput.runProcess(FIZZBUZZ_CMD + input);
-        assert process.getOutput().spread().equals(fizzBuzz(input) + "\n") || process.getOutput().spread().equals(fizzBuzz(input));
+        new StandardKdlTest("/sample", "FizzBuzz", fizzBuzzArguments, fizzBuzzOutputs);
     }
 
     public String fizzBuzz(int input) {
--- src/test/java/PrintArgTest.java
+++ src/test/java/PrintArgTest.java
@@ -1,12 +1,12 @@
 package test.java;
 
+import com.xarql.kdl.BestList;
 import com.xarql.kdl.CompilationDispatcher;
 import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.junit.jupiter.api.Test;
 
 public class PrintArgTest {
     public static final String[] ARGS = { "hello", "test" };
-    public static final String PRINTARG_CMD = StandardKdlTest.JAVA_CMD + "/basics PrintArg ";
 
     public static void main(String[] args) {
         new PrintArgTest().testPrintArg();
@@ -14,22 +14,8 @@ public class PrintArgTest {
 
     @Test
     public void testPrintArg() {
-        try {
-            // compile FizzBuzz.kdl
-            new CompilationDispatcher(new RegexFileFilter("PrintArg.kdl"), true).compileAll();
-
-            for(String arg : ARGS)
-                printArgCase(arg);
-
-        } catch(Exception e) {
-            e.printStackTrace();
-            assert false;
-        }
-    }
-
-    private void printArgCase(String arg) throws Exception {
-        ProcessOutput process = ProcessOutput.runProcess(PRINTARG_CMD + arg);
-        assert process.getOutput().spread().equals(arg + "\n") || process.getOutput().spread().equals(arg);
+        BestList<String> printArgArguments = new BestList<>(ARGS);
+        new StandardKdlTest("/basics", "PrintArg", printArgArguments, printArgArguments);
     }
 
 }
