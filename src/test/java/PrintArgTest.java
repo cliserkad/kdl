d765b7f47fb6f9b95c673e28406651f1ae66a98f
cliserkad <bryanchristopherjohnson@gmail.com>
2020-08-14 18:01:39 -0400

    Add PrintArgTest

---
+++ src/test/java/PrintArgTest.java
@@ -0,0 +1,37 @@
+package test.java;
+
+import com.xarql.kdl.CompilationDispatcher;
+import org.apache.commons.io.filefilter.RegexFileFilter;
+import org.junit.jupiter.api.Test;
+
+import static test.java.HelloWorldTest.JAVA_CMD;
+
+public class PrintArgTest {
+    public static final String[] ARGS = { "hello", "test" };
+    public static final String PRINTARG_CMD = JAVA_CMD + "/basics PrintArg ";
+
+    public static void main(String[] args) {
+        new PrintArgTest().testPrintArg();
+    }
+
+    @Test
+    public void testPrintArg() {
+        try {
+            // compile FizzBuzz.kdl
+            new CompilationDispatcher(new RegexFileFilter("PrintArg.kdl"), true).compileAll();
+
+            for(String arg : ARGS)
+                printArgCase(arg);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            assert false;
+        }
+    }
+
+    private void printArgCase(String arg) throws Exception {
+        ProcessOutput process = ProcessOutput.runProcess(PRINTARG_CMD + arg);
+        assert process.getOutput().spread().equals(arg + "\n") || process.getOutput().spread().equals(arg);
+    }
+
+}
