70b4fe968706759363df8c06da4ae3f95e1948e2
cliserkad <cliserkad@xarql.com>
2022-12-09 20:20:41 -0800

    Fix build. Refactor KDLCompilerPlugin to use Mojo Annotations. Fix dependency scope in kdl-maven-plugin pom.xml. Fix antlr4-runtime version in kdl-parent pom.xml.

--- kdl-maven-plugin/src/main/java/com/xarql/kdl/plugin/KDLCompilerPlugin.java
+++ kdl-maven-plugin/src/main/java/com/xarql/kdl/plugin/KDLCompilerPlugin.java
@@ -3,33 +3,52 @@ package com.xarql.kdl.plugin;
 import com.xarql.kdl.CompilationDispatcher;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.*;
 
 import java.io.File;
 
 /**
- * Goal which touches a timestamp file.
- *
- * @goal kdl
- * @phase compile
+ * Compiles KDL files in a Maven project
  */
+@Mojo(
+		name = "kdl",
+		defaultPhase = LifecyclePhase.COMPILE,
+		requiresDependencyResolution = ResolutionScope.TEST,
+		requiresOnline = false,
+		requiresProject = true
+)
+@Execute(
+		goal = "kdl",
+		phase = LifecyclePhase.COMPILE
+)
 public class KDLCompilerPlugin extends AbstractMojo {
 
 	/**
-	 * Location of the file.
-	 *
-	 * @parameter property="project.build.directory"
-	 * @required
+	 * Location of compiled (output) .class files
 	 */
+	@Parameter(
+			name ="outputDirectory",
+			property ="project.build.directory",
+			required = true,
+			readonly = false
+	)
 	private File outputDirectory;
 
 	/**
-	 * Location of .kdl files
-	 *
-	 * @parameter property="project.build.sourceDirectory"
-	 * @required
+	 * Location of source (input) .kdl files
 	 */
+	@Parameter(
+			name = "sourceDirectory",
+			property = "project.build.sourceDirectory",
+			required = true,
+			readonly = false
+	)
 	private File sourceDirectory;
 
+	/**
+	 * Compile files
+	 * @throws MojoFailureException Forwarded exceptions from compiler
+	 */
 	public void execute() throws MojoFailureException {
 		// append a slash to the file path if it isn't a directory
 		if(!outputDirectory.isDirectory())
